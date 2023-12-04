2f08612c262a777b7257ff0d98ccded977dcaaa3
cala18 <brayancala2375@gmail.com>
2023-12-04 10:22:39 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el multiplicando:");
+        int multiplicando = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el multiplicador:");
+        int multiplicador = Convert.ToInt32(Console.ReadLine());
+        int resultado = MultiplicacionRusa(multiplicando, multiplicador);
+        Console.WriteLine($"El resultado de la multiplicación es: {resultado}");
+    }
+    static int MultiplicacionRusa(int multiplicando, int multiplicador)
+    {
+        int resultado = 0;
+
+        while (multiplicador > 0)
+        {
+            if (multiplicador % 2 != 0)
+            {
+                resultado += multiplicando;
+            }
+            multiplicando *= 2;
+            multiplicador /= 2;
+        }
+        return resultado;
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# multiplicacion rusa 
+en este proyecto se crea un programa donde se hace efectiva la multiplicacio rusa
+----
+Donde se ingresa un miltiplicando que sera multiplicado por 2 y un multiplicador que tambien sera dividio por 2 hasta que su valir sea uno, despues se suman  todos los multiplicandos correspondientes  a los multiplicadores impares 
